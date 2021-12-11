8e19a5683d58d0efaf781dce25eb93ce42991136
曾扬 <pseudonymgeanmu@163.comgit config --global user.email pseudonymgeanmu@163.com>
2021-12-11 10:54:26 +0800

    login功能结束

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,27 +1,148 @@
 <template>
-    <div class="login_container">
-        <div class="login_box"></div>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/cart.png" alt="" />
+      </div>
+
+      <!-- 登录表单区域 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input prefix-icon="el-icon-s-custom" v-model.trim="loginForm.username" 
+          @keyup.enter.native="login"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <!-- v-model.trim中的trim指的是输入框中无法输入空格 -->
+          <el-input prefix-icon="el-icon-lock" v-model.trim="loginForm.password" 
+          @keyup.enter.native="login" type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 这是登录表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456',
+      },
+      // 这是表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur',
+          },
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          {
+            min: 6,
+            max: 15,
+            message: '长度在 6 到 15 个字符',
+            trigger: 'blur',
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    // 点击重置按钮，重置登录表单
+    resetLoginForm() {
+      // console.log(this);
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      // 登录前的表单预验证
+      // 调用validate回调函数 箭头函数中valid返回True代表表单校验成功
+      this.$refs.loginFormRef.validate(async (valid) => {
+        // valid值为false-直接return-不发起请求
+        // 如果没有return-valid值为true-发起登录请求
+        if (!valid) return
+        const { data: res } = await this.$axios.post('login', this.loginForm)
+        // console.log(res)
+        if (res.meta.status !== 200) return this.$message.error('登录失败！')
+        // this.$message.success('登录成功')
+        this.$message({
+          message: '登录成功！',
+          type: 'success',
+          duration: 1000 //提示框显示时间改为1秒，默认值3秒
+        })
+        // 1.将登录成功之后的token，保存到客户端的sessionStorage中
+        //      1.1 项目中除了登录之外的其他API接口，必须在登录之后才能访问
+        //      1.2 token只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+        // localStorage生命周期是永久，这意味着除非用户显示在浏览器提供的UI上清除localStorage信息，否则这些信息将永远存在。
+        // sessionStorage生命周期为当前窗口或标签页，一旦窗口或标签页被永久关闭了，那么所有通过sessionStorage存储的数据也就被清空了。
+        window.sessionStorage.setItem('token', res.data.token)
+        //将res.data.token赋值给sessionStorage中的token
+        // 2.通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
+      })
+    },
+  },
+}
 </script>
 
 <!-- 代表着使用的是less语法格式，scoped代表这个样式只在当前组件内生效 -->
 <style lang="less" scoped>
-.login_container{
-    background-color: #2b4b6b;
-    height:100%
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
 }
-.login_box{
-    width: 450px;
-    height: 300px;
-    background-color: #fff;
-    border-radius: 3px;
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
     position: absolute;
     left: 50%;
-    top: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
