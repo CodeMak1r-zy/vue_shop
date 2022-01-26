f52ef42784fe9967b5c07d50831f11b96f2e06b2
曾扬 <pseudonymgeanmu@163.comgit config --global user.email pseudonymgeanmu@163.com>
2022-01-26 16:06:52 +0800

    完成了数据报表的开发

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Edit from '../components/goods/Edit.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -38,7 +39,8 @@ const routes = [
       { path: '/goods', component: GoodsList, },
       { path: '/goods/add', component: Add },
       { path: '/goods/edit', name: 'edit', component: Edit },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
     ]
   }
 ]
